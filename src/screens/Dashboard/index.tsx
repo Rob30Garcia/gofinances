297914df742f556dc0a4a9455bbe12fef11dada3
Robert Garcia <rgs2@icomp.ufam.edu.br>
2021-10-16 16:20:04 -0400

    feat: Create Header with user

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,11 +1,33 @@
 import React from 'react';
 
-import { Container, Title } from './styles';
+import { 
+  Container, 
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  User,
+  UserGretting,
+  UserName,
+  Icon
+} from './styles';
 
 export function Dashboard() {
   return (
     <Container>
-      <Title>Dashboard</Title>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo source={{ uri: 'https://avatars.githubusercontent.com/u/50270104?v=4'}}/>
+            <User>
+              <UserGretting >Olá,</UserGretting>
+              <UserName>Robert</UserName>
+            </User>
+          </UserInfo>
+
+          <Icon name="power" />
+        </UserWrapper>
+      </Header>
     </Container>
   );
 }
--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,15 +1,59 @@
 import styled from 'styled-components/native';
+import { RFPercentage, RFValue } from 'react-native-responsive-fontsize';
+import { Feather } from '@expo/vector-icons';
 
 export const Container = styled.View`
   flex: 1;
-  justify-content: center;
-  align-items: center;
 
   background: ${({theme}) => theme.colors.background};
 `;
 
-export const Title = styled.Text`
+export const Header = styled.View`
+  width: 100%;
+  height: ${RFPercentage(35)}px;
+  padding-top: 58px;
+  
+  background-color: ${({theme}) => theme.colors.primary};
+`;
+
+export const UserWrapper = styled.View`
+  flex-direction: row;
+  
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 24px;
+`;
+
+export const UserInfo = styled.View`
+  flex-direction: row;
+  align-items: center;
+  height: ${RFValue(48)}px;
+`;
+
+export const Photo = styled.Image`
+  width: ${RFValue(48)}px;
+  height: ${RFValue(48)}px;
+
+  border-radius: 10px;
+`;
+
+export const User = styled.View`
+  margin-left: 16px;
+`;
+
+export const UserGretting = styled.Text`
+  font-size: ${RFValue(18)}px;
+  font-family: ${({ theme }) => theme.fonts.regular};
+  color: ${({ theme }) => theme.colors.shape};
+`;
+
+export const UserName = styled.Text`
+  font-size: ${RFValue(18)}px;
   font-family: ${({ theme }) => theme.fonts.bold};
-  font-size: 24px;
-  color: ${({ theme }) => theme.colors.title};
-`; 
+  color: ${({ theme }) => theme.colors.shape}
+`;
+
+export const Icon = styled(Feather)`
+  font-size: ${RFValue(24)}px;
+  color: ${({theme}) => theme.colors.secondary};
+`;
