3654a1099dd1332a4380e2b44928998d6b31ffaa
Robert Garcia <rgs2@icomp.ufam.edu.br>
2021-10-16 22:43:20 -0400

    style: add change style for highlight cards

--- src/components/HighlightCard/index.tsx
+++ src/components/HighlightCard/index.tsx
@@ -10,18 +10,40 @@ import {
   LastTransaction,
 } from './styles';
 
-export function HighlightCard() {
+interface HighlightProps {
+  type: "up" | "down" | "total";
+  title: string;
+  amount: string;
+  lastTransaction: string;
+}
+
+const icons = {
+  up: "arrow-up-circle",
+  down: "arrow-down-circle",
+  total: "dollar-sign"
+}
+
+export function HighlightCard({
+  type,
+  title,
+  amount,
+  lastTransaction
+}) {
   return (
-    <Container>
+    <Container type={type}>
       <Header>
-        <Title>Entradas</Title>
-        <Icon name="arrow-up-circle"/>
+        <Title type={type}>
+          {title}
+        </Title>
+        <Icon name={icons[type]} type={type}/>
       </Header>
 
       <Footer>
-        <Amount>R$ 17.400,00</Amount>
-        <LastTransaction>
-          Última entrada dia 13 de abril
+        <Amount type={type}>
+          {amount}
+        </Amount>
+        <LastTransaction type={type}>
+          {lastTransaction}
         </LastTransaction>
       </Footer>
     </Container>
--- src/components/HighlightCard/styles.ts
+++ src/components/HighlightCard/styles.ts
@@ -1,10 +1,16 @@
-import styled from 'styled-components/native';
+import styled, { css } from 'styled-components/native';
 import { Feather } from '@expo/vector-icons';
 import { RFValue } from 'react-native-responsive-fontsize';
 
-export const Container = styled.View`
+interface TypeProps {
+  type: 'up' | 'down' | 'total';
+}
+
+export const Container = styled.View<TypeProps>`
   width: ${RFValue(300)}px;
-  background: ${({ theme }) =>  theme.colors.shape};
+  background: ${({ theme, type }) =>  
+    type === 'total' ? theme.colors.secondary : theme.colors.shape   
+  };
 
   padding: 20px;
   padding-bottom: ${RFValue(42)}px;
@@ -18,29 +24,49 @@ export const Header = styled.View`
   justify-content: space-between;
 `;
 
-export const Title = styled.Text`
+export const Title = styled.Text<TypeProps>`
   font-family: ${({ theme }) => theme.fonts.regular};
   font-size: ${RFValue(14)}px;
-  color: ${({ theme }) => theme.colors.title};
+  color: ${
+    ({ theme, type }) => 
+      type === 'total' ? theme.colors.shape : theme.colors.title 
+  };
 `;
 
-export const Icon = styled(Feather)`
-  color: ${({ theme }) => theme.colors.title};
+export const Icon = styled(Feather)<TypeProps>`
   font-size: ${RFValue(40)}px;
+
+  ${({ type }) => type === 'up'&& css`
+    color: ${({ theme }) => theme.colors.success};
+  `}
+
+  ${({ type }) => type === 'down'&& css`
+    color: ${({ theme }) => theme.colors.attention};
+  `}
+
+  ${({ type }) => type === 'total'&& css`
+    color: ${({ theme }) => theme.colors.shape};
+  `}
 `;
 
 export const Footer = styled.View`
   margin-top: 38px;
 `;
 
-export const Amount = styled.Text`
-  color: ${({ theme }) => theme.colors.title};
+export const Amount = styled.Text<TypeProps>`
+  color: ${
+    ({ theme, type }) => 
+      type === 'total' ? theme.colors.shape : theme.colors.title
+  };
   font-family: ${({ theme }) => theme.fonts.medium};
   font-size: ${RFValue(32)}px;
 `;
 
-export const LastTransaction = styled.Text`
-  color: ${({ theme }) => theme.colors.texts};
+export const LastTransaction = styled.Text<TypeProps>`
+  color: ${
+    ({ theme, type }) => 
+      type === 'total' ? theme.colors.shape : theme.colors.texts 
+  };
   font-family: ${({ theme }) => theme.fonts.regular};
   font-size: ${RFValue(12)}px;
 `;
