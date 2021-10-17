018da0d0ef3ea810d5043392026b5da618c4586a
Robert Garcia <rgs2@icomp.ufam.edu.br>
2021-10-17 12:08:41 -0400

    feat: Create list of transactions

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -12,23 +12,35 @@ import {
   Icon,
   Highlights,
   Transactions,
-  Title
+  Title,
+  TransactionList
 } from './styles';
 
 import { HighlightCard } from '../../components/HighlightCard';
 import { TransactionCard } from '../../components/TransactionCard';
 
 export function Dashboard() {
-  const data = {
-    type: 'positive',
-    title: 'Desenvolvimento de site',
-    amount: 'R$ 12.000,00',
-    category: {
-      name: 'Vendas',
-      icon: 'dollar-sign'
-    },
-    date: '13/04/2020'
-  }
+  const data = [{
+      type: 'positive',
+      title: 'Desenvolvimento de site',
+      amount: 'R$ 12.000,00',
+      category: {
+        name: 'Vendas',
+        icon: 'dollar-sign'
+      },
+      date: '13/04/2020'
+    }, 
+    {
+      type: 'negative',
+      title: 'Hamburgueria Pizzy',
+      amount: 'R$ 59,00',
+      category: {
+        name: 'Alimentação',
+        icon: 'coffee'
+      },
+      date: '10/04/2020'
+    }
+  ]
 
   return (
     <Container>
@@ -72,7 +84,13 @@ export function Dashboard() {
       <Transactions>
         <Title>Listagem</Title>
 
-        <TransactionCard data={data} />
+        <TransactionList 
+          data={data} 
+          keyExtractor={( item,  index ) => { return String(index) }}
+          renderItem={({ item }) => 
+            <TransactionCard data={item} />
+          }
+        />
 
       </Transactions>
     </Container>
