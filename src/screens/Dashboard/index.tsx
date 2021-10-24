36be239b238839b1266921824447e03c85bfc2fb
Robert Garcia <rgs2@icomp.ufam.edu.br>
2021-10-24 12:47:16 -0400

    chore: set value in the Highlight cards

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -26,8 +26,19 @@ export interface DataListProps extends TransactionCardProps {
   id: string;
 }
 
+interface HighlightProps {
+  amount: string;
+}
+
+interface HighlightData {
+  entries: HighlightProps;
+  expensive: HighlightProps;
+  total: HighlightProps; 
+}
+
 export function Dashboard() {
   const [data, setData] = useState<DataListProps[]>([]);
+  const [highlightData, setHighlightData] = useState<HighlightData>({} as HighlightData);
 
   async function loadTransactions() {
     const dataKey = '@gofinances:transactions';
@@ -37,8 +48,16 @@ export function Dashboard() {
     //typing transactions
     const transactions = response ? JSON.parse(response) : [];    
 
+    let entriesSum = 0;
+    let expensiveSum = 0;
+
     const transactionsFormatted: DataListProps[] = transactions
-      .map((item: DataListProps) => {        
+      .map((item: DataListProps) => {   
+        
+        item.type === 'positive' ? 
+          entriesSum += Number(item.amount) : 
+          expensiveSum += Number(item.amount);
+        
         const amount = Number(item.amount).toLocaleString('pt-BR', {
           style: 'currency',
           currency: 'BRL'
@@ -61,6 +80,29 @@ export function Dashboard() {
       });
 
     setData(transactionsFormatted);      
+
+    const total = entriesSum - expensiveSum;
+
+    setHighlightData({
+      entries: {
+        amount: entriesSum.toLocaleString('pt-BR', {
+          style: 'currency',
+          currency: 'BRL'
+        })
+      },
+      expensive: {
+        amount: expensiveSum.toLocaleString('pt-BR', {
+          style: 'currency',
+          currency: 'BRL'
+        })
+      },
+      total: {
+        amount: total.toLocaleString('pt-BR', {
+          style: 'currency',
+          currency: 'BRL'
+        })
+      }
+    })
   } 
 
   useEffect(() => {
@@ -93,21 +135,21 @@ export function Dashboard() {
         <HighlightCard
           type="up"
           title="Entradas"
-          amount="R$ 17.400,00"
+          amount={highlightData.entries.amount}
           lastTransaction="Última entrada dia 13 de abril"
         />
 
         <HighlightCard
           type="down"
           title="Saídas"
-          amount="R$ 1.259,00"
+          amount={highlightData.expensive.amount}
           lastTransaction="Última saída dia 03 de abril"
         />
 
         <HighlightCard 
           type="total"
           title="Total"
-          amount="R$ 16.141,00"
+          amount={highlightData.total.amount}
           lastTransaction="01 à 16 de abril"
         />
       </Highlights>
